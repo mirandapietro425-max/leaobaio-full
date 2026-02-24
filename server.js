5c11a78a3ec87b3a9af67d8469280b7d5dba62a6
mirandapietro425-max <mirandapietro425@gmail.com>
2026-02-24 18:47:25 -0300

    update: descrição

--- server.js
+++ server.js
@@ -1096,8 +1096,7 @@ app.get('/sitemap.xml', async (req, res) => {
     res.setHeader('Content-Type', 'application/xml');
     res.send(`<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-${urls.join('
-')}
+${urls.join('\n')}
 </urlset>`);
   } catch (e) {
     res.status(500).send('Erro ao gerar sitemap.');
